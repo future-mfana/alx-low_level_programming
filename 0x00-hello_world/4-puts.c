d132a224c151f2cce2dbab76022904d64c3d9a7e
Future-mfana <Siphiweb61@gmail.com>
2023-03-21 15:14:18 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+include <studio.h>
+
+/** 
+ * main - this is the main function
+ *
+ * Retun: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzles");
+	return (0);
