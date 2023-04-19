bf11ac4e21fa7296ca8a40ba67710a3543fc4bde
Future-mfana <Siphiweb61@gmail.com>
2023-04-19 02:31:51 -0700

    1.A dog is the only thing that loves you more than you love yourself.

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  * @age: The age of the dog.
  * @owner: The name of the owner.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
@@ -17,4 +17,3 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 		d->owner = owner;
 	}
 }
-
