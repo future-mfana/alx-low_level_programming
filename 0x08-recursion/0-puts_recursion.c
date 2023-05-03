36c664192e27a0d63c87b8d156071c5a934a4fb0
Future-mfana <Siphiweb61@gmail.com>
2023-05-03 01:21:48 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - a function like put()
- * @s: input
- *
- * Return: Always 0 (if success)
+ * _puts_recursion - prints a string and a new line.
+ * @s: input string
  */
-void _put_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
-		_put_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
-
 	else
+	{
 		_putchar('\n');
+	}
 }
+
