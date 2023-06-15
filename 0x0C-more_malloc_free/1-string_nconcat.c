15d5247f29803e843588811b34ab0a656acb153a
future-mfana <Siphiweb61@gmail.com>
2023-06-15 01:35:10 -0700

    2._calloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * *string_nconcat - concatenates n bytes of a string to another
+ * *string_nconcat - concatenates n bytes of a string to another string
  * @s1: string to append to
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenate to s1
